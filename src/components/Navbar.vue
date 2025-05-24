d9f45eef4c108e9211ea5b89d1342064e76289f2
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-05-24 08:11:47 +0100

    :star: feat: add landing page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand text-primary" href="#">DraftMaster</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+        <button class="btn btn-outline-primary rounded-pill px-4 py-2" @click="navigateTo('/login')">
+          Start Writing 📝<i class="fas fa-arrow-up-right-from-square ms-2"></i>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+// import { useRouter } from 'vue-router';
+
+// const router = useRouter();
+
+// const navigateTo = (path) => {
+//   router.push(path);
+// };
+</script>
